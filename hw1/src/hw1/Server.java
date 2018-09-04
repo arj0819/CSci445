9afbc8a3aa6ca6296d7aaae9aeee1e28c1b5d214
arj0819 <aaronandsydney0819@gmail.com>
2018-09-03 23:33:30 -0500

    Build: Adds basic method structure to Server.java

--- hw1/src/hw1/Server.java
+++ hw1/src/hw1/Server.java
@@ -2,8 +2,25 @@ package hw1;
 
 public class Server {
 
+    private boolean occupied = false;
+    private Patient patientBeingServed;
+
     public Server() {
         
+    }
+
+    public void service(Patient patient) {
+        this.patientBeingServed = patient;
+        occupied = true;
+    }
+
+    public Patient discharge() {
+        occupied = false;
+        return patientBeingServed;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
     }
 
 }
