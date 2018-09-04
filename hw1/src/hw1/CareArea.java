50608355205c8d99394eae1c7f0ff12f00004830
arj0819 <aaronandsydney0819@gmail.com>
2018-09-03 23:18:45 -0500

    Build: Adds constructor functionality in CareArea.java to instantiate number of Servers required by any type of CareArea

--- hw1/src/hw1/CareArea.java
+++ hw1/src/hw1/CareArea.java
@@ -2,10 +2,13 @@ package hw1;
 
 import java.util.Queue;
 import java.util.PriorityQueue;
+import java.util.List;
+import java.util.ArrayList;
 
 public abstract class CareArea implements Hospitable {
 
     private Queue patientQueue = new PriorityQueue<Patient>();
+    private List availableServers = new ArrayList<Server>();
 
     private int maxInterArrivalTime;
     private int maxServiceTime;
@@ -17,10 +20,15 @@ public abstract class CareArea implements Hospitable {
     private int averagePatientsInQueue;
 
 
-    public CareArea(int maxInterArrivalTime, int maxServiceTime) {
-        
+    public CareArea(int maxInterArrivalTime, int maxServiceTime, int numberOfServers) {
+        this.maxInterArrivalTime = maxInterArrivalTime;
+        this.maxServiceTime = maxServiceTime;
+
+        for (int i = 0; i < numberOfServers; i++) {
+            availableServers.add(new Server());
+        }
     }
-    
+
     public boolean recievePatient() {
         return true;
     }
--- hw1/src/hw1/Triage.java
+++ hw1/src/hw1/Triage.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 
 public class Triage extends CareArea {
 
-    public Triage(int maxInterArrivalTime, int maxServiceTime) {
-        super(maxInterArrivalTime, maxServiceTime);
+    public static final int NUM_OF_SERVERS = 1;
+
+    public Triage(int maxInterArrivalTime, int maxServiceTime, int numberOfServers) {
+        super(maxInterArrivalTime, maxServiceTime, NUM_OF_SERVERS);
     }
 
 }
