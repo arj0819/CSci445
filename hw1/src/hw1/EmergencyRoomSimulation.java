fb5a3c5180595747df010cf5198f312c207d5d55
arj0819 <aaronandsydney0819@gmail.com>
2018-09-07 00:10:24 -0500

    Build: Completes hw1 for now. Makes a huge mess of the code, but works. Will fix later if needed

--- hw1/src/hw1/EmergencyRoomSimulation.java
+++ hw1/src/hw1/EmergencyRoomSimulation.java
@@ -8,30 +8,28 @@ import java.util.NoSuchElementException;
 
 public class EmergencyRoomSimulation {
 
-    private static double currentMinute = 0;
     private static double nextArrivalTime;
     private static double nextDischargeTime;
 
     private static double expectedAvgInterArrivalTimePerPatient;
     private static double expectedAvgServiceTimePerPatient;
     private static int maxQueueLength;
+    private static int maxDischarges;
 
     private static boolean endSimulation = false;
 
     private static List<CareArea> availableCareAreas = new ArrayList<CareArea>();
 
+    private static List<Integer> patientsInQueueList = new ArrayList<Integer>();
+
     private static Random rand = new Random();
 
     public static void main(String[] args) {
 
-        for (String arg : args) {
-            System.out.println(arg);
-        }
-
-
         expectedAvgInterArrivalTimePerPatient = Double.parseDouble(args[0]);
         expectedAvgServiceTimePerPatient = Double.parseDouble(args[1]);
         maxQueueLength = Integer.parseInt(args[2]);
+        maxDischarges = Integer.parseInt(args[3]);
 
         CareArea triageRoom = new Triage(expectedAvgInterArrivalTimePerPatient, 
                                          expectedAvgServiceTimePerPatient, 
@@ -39,57 +37,112 @@ public class EmergencyRoomSimulation {
 
         availableCareAreas.add(triageRoom);
 
-        while (!endSimulation) {
+        EventScheduler eventScheduler = new EventScheduler(expectedAvgInterArrivalTimePerPatient, 
+                                                           expectedAvgServiceTimePerPatient,
+                                                           maxDischarges);
 
-            if (nextArrivalTime <= nextDischargeTime) {
-                nextArrivalTime = Math.log(1-rand.nextDouble())/(-1/expectedAvgServiceTimePerPatient) + currentMinute;
-                System.out.println("1");
-                if (!triageRoom.patientArrival(new Patient(triageRoom.getShortestWaitTime(currentMinute)))) {
-                    System.out.println("Patient queue is full. Ending Simulation.");
-                    endSimulation = true;
-                    System.out.println("2");
-                    continue;
-                }
-                System.out.println("3");
-                currentMinute = nextArrivalTime;
-                System.out.println(currentMinute);
+        // for (int i = 0; i < 50; i++) {
+        //     eventScheduler.generateNextEventSequence();
+        //     //currentMinute += eventScheduler.getNextEvent().getEventTime();
+        // }
 
-                try {
-                    nextDischargeTime = triageRoom.servicePatient(triageRoom.receivePatient(), currentMinute);
-                } catch (NoSuchElementException e) {
-                    System.out.println("The queue is empty at the moment.");
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-            } else {
+        while (!eventScheduler.generateNextEventSequence()){}
 
-                System.out.println("4");
-                try {
-                    Server serverReadyToDischarge = triageRoom.getServerReadyToDischarge(nextDischargeTime);
-                    serverReadyToDischarge.clearPatientForDischarge();
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                    e.printStackTrace();
-                }
-        
-                try {
-                    triageRoom.dischargePatient();
-                } catch (NoSuchElementException e) {
-                    System.out.println(e.getMessage());
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                    e.printStackTrace();
-                }
-                System.out.println("5");
-    
-                if (triageRoom.calculateTotalPatientsServed() >= 100) {
-                    endSimulation = true;
-                }
+        //eventScheduler.printEventLists();
+
+        double simulationLength = 0.0;
+
+        int patientCounter = 0;
+        double totalPatientsInQueue = 0;
+        double avgPatientsInQueue = 0.0;
+
+        while (eventScheduler.getEventQueue().size() != 0) {
+            Event currentEvent = eventScheduler.getEventQueue().remove();
+            //System.out.println("-------------\nEVENT TYPE: " + currentEvent.getEventType() + "\nEVENT TIME: "+currentEvent.getEventTime()+ "\nEVENT DURATION: "+currentEvent.getEventDuration());
+            if (eventScheduler.getEventQueue().size() == 0) {
+                simulationLength = currentEvent.getEventTime();
             }
+
+            if (currentEvent.getEventType().equals("Arrival")) {
+                patientCounter++;
+            } else {
+                patientCounter--;
+            }
+            patientsInQueueList.add(patientCounter);
         }
 
+        System.out.println("\n(All units are in minutes)");
+        eventScheduler.printAvgWaitTime();
+        eventScheduler.printActualAvgInterArrivalTime();
+        eventScheduler.printActualAvgServiceTime();
+        System.out.printf("Simulation Length: %.3f\n", simulationLength);
 
-        System.out.println(triageRoom.report());
+        for (int i = 0; i < patientsInQueueList.size(); i++) {
+            totalPatientsInQueue += patientsInQueueList.get(i);
+        }
+        avgPatientsInQueue = totalPatientsInQueue / patientsInQueueList.size();
+        System.out.printf("Average Patients In Queue: %.3f\n", avgPatientsInQueue);
+        System.out.printf("Patients Still In Queue: %d", patientCounter);
+    // public void printAvgPatientsWaitingInQueue() {
+
+    // }
+
+    // public void printNumOfPatientsLeftInQueue() {
+
+    // }
+
+
+        // while (!endSimulation) {
+
+        //     if (nextArrivalTime <= nextDischargeTime) {
+        //         nextArrivalTime = Math.log(1-rand.nextDouble())/(-1/expectedAvgServiceTimePerPatient) + currentMinute;
+        //         System.out.println("1");
+        //         if (!triageRoom.patientArrival(new Patient(triageRoom.getShortestWaitTime(currentMinute)))) {
+        //             System.out.println("Patient queue is full. Ending Simulation.");
+        //             endSimulation = true;
+        //             System.out.println("2");
+        //             continue;
+        //         }
+        //         System.out.println("3");
+        //         currentMinute = nextArrivalTime;
+        //         System.out.println(currentMinute);
+
+        //         try {
+        //             nextDischargeTime = triageRoom.servicePatient(triageRoom.receivePatient(), currentMinute);
+        //         } catch (NoSuchElementException e) {
+        //             System.out.println("The queue is empty at the moment.");
+        //         } catch (Exception e) {
+        //             System.out.println(e.getMessage());
+        //         }
+        //     } else {
+
+        //         System.out.println("4");
+        //         try {
+        //             Server serverReadyToDischarge = triageRoom.getServerReadyToDischarge(nextDischargeTime);
+        //             serverReadyToDischarge.clearPatientForDischarge();
+        //         } catch (Exception e) {
+        //             System.out.println(e.getMessage());
+        //             e.printStackTrace();
+        //         }
+        
+        //         try {
+        //             triageRoom.dischargePatient();
+        //         } catch (NoSuchElementException e) {
+        //             System.out.println(e.getMessage());
+        //         } catch (Exception e) {
+        //             System.out.println(e.getMessage());
+        //             e.printStackTrace();
+        //         }
+        //         System.out.println("5");
+    
+        //         if (triageRoom.calculateTotalPatientsServed() >= 100) {
+        //             endSimulation = true;
+        //         }
+        //     }
+        // }
+
+
+        //System.out.println(triageRoom.report());
 
     }
 }
--- hw1/src/hw1/Hospitable.java
+++
@@ -1,9 +0,0 @@
-package hw1;
-
-public interface Hospitable {
-
-    public Patient receivePatient();
-    public boolean servicePatient(Server server, Patient nextPatient);
-    public void dischargePatient(Server server);
-
-}
