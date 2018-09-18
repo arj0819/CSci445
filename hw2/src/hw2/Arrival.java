455e419b9380114d6416ecb0b519ac2eafad085c
arj0819 <aaronandsydney0819@gmail.com>
2018-09-18 14:58:58 -0500

    Build: Adds Arrival and Departure IDs to keep track of which event took place in each CareArea

--- hw2/src/hw2/Arrival.java
+++ hw2/src/hw2/Arrival.java
@@ -3,13 +3,18 @@ package hw2;
 public class Arrival extends Event {
 
     private static int totalArrivals = 0;
-
+    private int arrivalID = 0;
+    
     public Arrival(double timeOccurred, double timeEndured, double timeWaited) {
         super(timeOccurred, timeEndured, timeWaited);
-        totalArrivals++;
+        this.arrivalID = ++totalArrivals;
     }
 
     public static int getTotalArrivals() {
         return totalArrivals;
+    }
+
+    public int getArrivalID() {
+        return arrivalID;
     }
 }
--- hw2/src/hw2/Departure.java
+++ hw2/src/hw2/Departure.java
@@ -3,13 +3,18 @@ package hw2;
 public class Departure extends Event {
 
     private static int totalDepartures = 0;
+    private int departureID = 0;
 
     public Departure(double timeOccurred, double timeEndured) {
         super(timeOccurred, timeEndured);
-        totalDepartures++;
+        this.departureID = ++totalDepartures;
     }
 
     public static int getTotalDepartures() {
         return totalDepartures;
+    }
+
+    public int getDepartureID() {
+        return departureID;
     }
 }
