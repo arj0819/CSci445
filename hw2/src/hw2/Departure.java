ec1331d2863a91ec9f46495d2598b9f187046b40
arj0819 <aaronandsydney0819@gmail.com>
2018-09-21 23:16:42 -0500

    Build: Adds support for tracking Patients in Departure events

--- hw2/src/hw2/Departure.java
+++ hw2/src/hw2/Departure.java
@@ -5,11 +5,14 @@ public class Departure extends Event {
     private static int totalDepartures = 0;
     private int departureID = 0;
 
+    private int patientID = 0;
+
     private String destination = "";
 
-    public Departure(double timeDeparted, String location) {
+    public Departure(int patientID, double timeDeparted, String location) {
         super(timeDeparted, location);
         this.departureID = ++totalDepartures;
+        this.patientID = patientID;
         System.out.println("DEPARTURE "+this.departureID+" CONSTRUCTED");
     }
 
@@ -19,6 +22,10 @@ public class Departure extends Event {
 
     public int getID() {
         return departureID;
+    }
+
+    public int getPatientID() {
+        return patientID;
     }
 
     public String getDestination() {
