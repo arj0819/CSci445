cde52907bef77f5caf641f49741ed48e67ad9234
arj0819 <aaronandsydney0819@gmail.com>
2018-09-21 23:16:25 -0500

    Build: Adds support for tracking Patients in Arrival events

--- hw2/src/hw2/Arrival.java
+++ hw2/src/hw2/Arrival.java
@@ -4,13 +4,25 @@ public class Arrival extends Event {
 
     private static int totalArrivals = 0;
     private int arrivalID = 0;
+
+    private static int totalPatients = 0;
+    private int patientID = 0;
+
+
     private boolean transferRequired = false;
     
     public Arrival(double timeArrived, double interArrivalTime, double serviceTime, double waitTime, boolean transferRequired, String location) {
         super(timeArrived, interArrivalTime, serviceTime, waitTime, location);
         this.transferRequired = transferRequired;
         this.arrivalID = ++totalArrivals;
+        this.patientID = ++totalPatients;
         System.out.println("ARRIVAL "+this.arrivalID+" CONSTRUCTED");
+    }
+
+    public Arrival(int patientID, double timeArrived, double interArrivalTime, double serviceTime, double waitTime, boolean transferRequired, String location) {
+        super(timeArrived,interArrivalTime,serviceTime,waitTime,location);
+        this.arrivalID = ++totalArrivals;
+        this.patientID = patientID;
     }
 
     public static int getTotalArrivals() {
@@ -19,6 +31,10 @@ public class Arrival extends Event {
 
     public int getID() {
         return arrivalID;
+    }
+
+    public int getPatientID() {
+        return patientID;
     }
 
     public boolean mustTransfer() {
