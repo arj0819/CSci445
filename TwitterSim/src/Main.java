9ab190ffb0206241219f05651db71752070a3eff
arj0819 <aaronandsydney0819@gmail.com>
2018-12-05 01:51:59 -0600

    Build: Updates Account and Tweet to adhere to the most recent design ideas, creates a quick demonstration of Account and Tweet creation in Main

--- TwitterSim/src/Main.java
+++ TwitterSim/src/Main.java
@@ -1,34 +1,77 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
  
     private static List<Account> allAccounts = new ArrayList<Account>();
+    private static Random rand = new Random();
 
     public static void main(String[] args) {
         //Create initial accounts - # of initial Accounts should be an initial parameter
         for (int i = 0; i < 10; i++) {
-            //Every other account is a Bot. We should be able to decide how many
-            //Human and Bot accounts to start with.
-            if (i%2==0) {
-                //Create Human account
-                allAccounts.add(new Account(false));
-            } else {
-                //Create Bot account
-                allAccounts.add(new Account(true));
+            allAccounts.add(new Account(
+                                    NormalRNG(0.25,0.5),
+                                    NormalRNG(0.25,0.5),
+                                    exponRNG(2.0)
+                            ));
+        }
+        //Print all initial account stats
+        System.out.println("INITIAL STATISTICS\n\n"+
+                           "Total Accounts --> " + Account.totalAccounts +"\n"+
+                           "Total Tweets ----> " + Tweet.totalTweets + "\n");
+
+        for (int i = 0; i < allAccounts.size(); i++) {
+            System.out.println(allAccounts.get(i));
+        }
+
+        while (Tweet.totalTweets <= 100) {
+            for (int i = 0; i < allAccounts.size(); i++) {
+                double tweetProbThreshold = rand.nextDouble();
+                Account currentAccount = allAccounts.get(i);
+                double currentTweetProbability = currentAccount.getTweetProbability();
+                double currentAvgTweetQuality = exponRNG(currentAccount.getAvgTweetQuality());
+    
+                if (currentTweetProbability >= tweetProbThreshold) {
+                    currentAccount.generateTweet(currentAvgTweetQuality, currentAccount.getID());
+                }
             }
         }
 
-        //Print all account stats
-        System.out.println("Total # of Accounts: " + Account.totalAccounts +"\n");
-        for (int i = 0; i < allAccounts.size(); i++) {
-            System.out.println("Is Account " + allAccounts.get(i).getID() + " a bot? " + allAccounts.get(i).isBot());
-            System.out.println("   Account " + allAccounts.get(i).getID() + " Follower Count: "+allAccounts.get(i).getFollowerCount());
-            System.out.println("   Account " + allAccounts.get(i).getID() + " Tweet Count: "+allAccounts.get(i).getTweetCount());
-            System.out.println("   Account " + allAccounts.get(i).getID() + " Avg Tweet Quality: "+allAccounts.get(i).getAvgTweetQuality());
-            System.out.println("   Account " + allAccounts.get(i).getID() + " Popularity Multiplier: "+allAccounts.get(i).getPopularityMult()+"\n");
-        }
+        //Print all final account stats
+        System.out.println("FINAL STATISTICS\n\n"+
+                           "Total Accounts --> " + Account.totalAccounts +"\n"+
+                           "Total Tweets ----> " + Tweet.totalTweets + "\n");
 
+        for (int i = 0; i < allAccounts.size(); i++) {
+            Account currentAccount = allAccounts.get(i);
+            System.out.println(currentAccount);
+        }
+    }
+
+
+    public static double NormalRNG(int stdDev, int mean) {
+        double value = 0;
+        value = (rand.nextGaussian()*stdDev)+mean;
+        return value;
+    }
+
+    public static double NormalRNG(double stdDev, double mean) {
+        double value = 0;
+        value = (rand.nextGaussian()*stdDev)+mean;
+        if (value > 1.0) {
+            value = 1.0;
+        } else if (value < 0.0) {
+            value = 0.01;
+        }
+        return value;
+    }
+
+    public static double exponRNG(double mean) {
+        double lambda = 1/mean;
+        double value = 0;
+        value = Math.log(1-rand.nextDouble())/(-lambda);
+        return value;
     }
 
 }
--- TwitterSim/src/Tweet.java
+++ TwitterSim/src/Tweet.java
@@ -2,13 +2,44 @@ public class Tweet {
     
     public static int totalTweets = 0;
     private int tweetID;
+    private int tweetedByID;
+    private double quality;
 
-    public Tweet() {
+    public Tweet(double quality, int tweetedByID) {
         setID();
+        setQuality(quality);
+        setTweetedByID(tweetedByID);
     }
 
     private void setID() {
         this.tweetID = ++totalTweets;
+    }
+
+    public int getID() {
+        return this.tweetID;
+    }
+
+    private void setQuality(double quality) {
+        this.quality = quality;
+    }
+
+    public double getQuality() {
+        return this.quality;
+    }
+
+    public void setTweetedByID(int tweetedByID) {
+        this.tweetedByID = tweetedByID;
+    }
+
+    public int getTweetedByID() {
+        return this.tweetedByID;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("    Tweet " + getID() + "\n"+
+                             "      Tweeted By --> Account "+getTweetedByID()+"\n"+
+                             "      Quality -----> " + getQuality() + "\n");
     }
 
 }
